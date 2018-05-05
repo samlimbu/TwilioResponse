f451a47f4cee727e0bb4c4761bf363d957df2118
samlimbu <samlimbu@hotmail.com>
2018-05-05 19:14:19 +0545

    test

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get('/test/:id&:id2', (req,res)=>{
           });
 });
 
-app.use('/getParams',paramRouter)
+app.use('/getParams',paramRouter);
 
 app.use('/genre', genreRouter);
 app.post('/', (request, response) => {
